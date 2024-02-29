fb97d044030b8a32609a654ce5802dde7fc70669
Anzuhaa <akuendzu@gmail.com>
2024-02-29 08:29:26 +0700

    Update Refactor

--- app/src/main/java/com/example/recycleviewfood/item.java
+++ app/src/main/java/com/example/recycleviewfood/item.java
@@ -4,7 +4,6 @@ public class item {
 
     String name;
     String description;
-
      int image;
 
     public item(String name, String description, int image) {
@@ -13,27 +12,11 @@ public class item {
         this.image = image;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    public String getName() { return name; }
     public String getDescription() {
         return description;
     }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public int getImage() {
         return image;
-    }
-
-    public void setImage(int image) {
-        this.image = image;
     }
 }
